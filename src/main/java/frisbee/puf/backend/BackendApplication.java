3d190a31a90b66b324210660d0ea569d04f81a84
Diana Schilling <diana.maria.schilling@gmail.com>
2022-01-09 22:10:50 +0100

    First functioning web socket prototype

--- src/main/java/frisbee/puf/backend/BackendApplication.java
+++ src/main/java/frisbee/puf/backend/BackendApplication.java
@@ -1,14 +1,23 @@
 package frisbee.puf.backend;
 
-import org.springframework.boot.SpringApplication;
+import org.java_websocket.server.WebSocketServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+import java.net.InetSocketAddress;
 
 @SpringBootApplication (exclude = SecurityAutoConfiguration.class)
 public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+
+//        SpringApplication.run(BackendApplication.class, args);
+
+        String host = "localhost";
+        int port = 8080;
+        WebSocketServer server = new FrisbeeWebSocketServer(new InetSocketAddress(host, port));
+        server.run();
+
     }
 
 }
