80edbf12ceccdc4272145679bc6837f6def43957
Diana Schilling <diana.maria.schilling@gmail.com>
2022-01-23 11:43:46 +0100

    Add javadocs to BackendApplication

--- src/main/java/frisbee/puf/backend/BackendApplication.java
+++ src/main/java/frisbee/puf/backend/BackendApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BackendApplication {
 
+    /**
+     * Starts the backend application.
+     *
+     * @param args optional arguments that can be passed
+     */
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
