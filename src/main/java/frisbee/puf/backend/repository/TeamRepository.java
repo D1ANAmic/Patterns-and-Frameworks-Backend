a3385850a9ac7b78bab9325cdbd6bdb35273c1c8
Diana Schilling <diana.maria.schilling@gmail.com>
2022-01-23 11:40:17 +0100

    Add javadocs to TeamRepository

--- src/main/java/frisbee/puf/backend/repository/TeamRepository.java
+++ src/main/java/frisbee/puf/backend/repository/TeamRepository.java
@@ -7,13 +7,35 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface TeamRepository  extends JpaRepository<Team, Long> {
+public interface TeamRepository extends JpaRepository<Team, Long> {
+    /**
+     * Selects a Team record based on the provided email from
+     * the database.
+     *
+     * @param name the team's name
+     * @return a team object
+     */
     @Query("select t from Team t where t.name = ?1")
     List<Team> findByName(String name);
 
+    /**
+     * Selects a Team record based on the provided player from
+     * the database.
+     *
+     * @param player the player
+     * @return a team object
+     */
     @Query("select t from Team t where t.playerLeft = ?1 or t.playerRight = ?1")
     List<Team> findByPlayer(Player player);
 
-    @Query("select t from Team t where (t.playerLeft = ?1 or t.playerRight = ?1) and t.active = true")
+    /**
+     * Selects a Team record based on the provided player and the status
+     * active from the database.
+     *
+     * @param player the player
+     * @return a team object
+     */
+    @Query("select t from Team t where (t.playerLeft = ?1 or t.playerRight = "
+            + "?1) and t.active = true")
     List<Team> findActiveByPlayer(Player player);
 }
