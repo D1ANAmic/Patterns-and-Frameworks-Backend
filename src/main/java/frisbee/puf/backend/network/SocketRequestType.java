4753063ce7f8e3dffc4bc33404752c039262197f
Diana Schilling <diana.maria.schilling@gmail.com>
2022-01-23 11:42:15 +0100

    Add javadocs to SocketRequestType

--- src/main/java/frisbee/puf/backend/network/SocketRequestType.java
+++ src/main/java/frisbee/puf/backend/network/SocketRequestType.java
@@ -3,16 +3,35 @@ package frisbee.puf.backend.network;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum SocketRequestType {
+    /**
+     * The request type for initializing the game setup.
+     */
     @JsonProperty("INIT")
     INIT,
+    /**
+     * The request type for notifying the other client that player is ready.
+     */
     @JsonProperty("READY")
     READY,
+    /**
+     * The request type for informing the other client about character movement.
+     */
     @JsonProperty("MOVE")
     MOVE,
+    /**
+     * The request type for informing the other client that the frisbee was
+     * thrown.
+     */
     @JsonProperty("THROW")
     THROW,
+    /**
+     * The request type for starting the game.
+     */
     @JsonProperty("GAME_RUNNING")
     GAME_RUNNING,
+    /**
+     * The request type for disconnecting a client.
+     */
     @JsonProperty("DISCONNECT")
-    DISCONNECT;
+    DISCONNECT
 }
